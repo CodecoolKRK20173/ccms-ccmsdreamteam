6be0d579d09c2e369d98cfad82e31bc57c5e34ff
Michal Wisniewski <mihuwis@gmail.com>
2018-10-02 15:17:08 +0200

    Add method declaration methods: add/grade assigments methods: add/remove/edit student check Attendence

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -1,4 +1,34 @@
 package com.codecool.model;
 
 public class Mentor extends Employee {
+
+    public Mentor(){
+
+    }
+
+    public void addAssigmen(){
+
+    }
+
+    public boolean gradeAssigment(boolean isPositiveGrade){
+        return isPositiveGrade;
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void removeStudent(){
+
+    }
+
+    public void editStudent(){
+
+    }
+
+    public void checkAttendance(){
+        
+    }
+
+
 }
