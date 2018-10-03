7f83765739974af308ab47ff4680e2b6b3b89293
Michal Wisniewski <mihuwis@gmail.com>
2018-10-03 12:38:44 +0200

    Add MentrolController methods declarations

--- src/main/java/com/codecool/controller/MentorController.java
+++ src/main/java/com/codecool/controller/MentorController.java
@@ -1,4 +1,29 @@
 package com.codecool.controller;
 
 public class MentorController {
+
+
+    public void addAssigmen(){
+
+    }
+
+    public boolean gradeAssigment(boolean isPositiveGrade){
+        return isPositiveGrade;
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void removeStudent(){
+
+    }
+
+    public void editStudent(){
+
+    }
+
+    public void checkAttendance(){
+
+    }
 }
