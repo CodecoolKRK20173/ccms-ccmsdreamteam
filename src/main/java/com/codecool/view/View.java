d066a50422d7f69e5872ffe51ac7978e360e9132
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-10-03 11:52:54 +0200

    Implement RegularEmployeeController

--- src/main/java/com/codecool/view/View.java
+++ src/main/java/com/codecool/view/View.java
@@ -1,6 +1,7 @@
 package com.codecool.view;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class View {
@@ -8,6 +9,10 @@ public class View {
     public void printMainMenu() {
         printHeader();
         System.out.println("1.Log In\n2.Exit");
+        printGetOption();
+    }
+    public void printRegularEmployeeMenu() {
+        System.out.println("1.See list of students\n2.Exit");
         printGetOption();
     }
     private void printHeader() {
@@ -34,5 +39,15 @@ public class View {
     }
     public void printErrorInputMessage() {
         System.out.println("Provide proper input!");
+    }
+    public void printWelcomeUser(String user) {
+        System.out.println("Welcome" + user + "!");
+    }
+    public void printListOfStudents(List<List<String>> listOfStudents) { // TODO - nice table with students
+        for (List<String> student : listOfStudents) {
+            for (String name : student) {
+                System.out.println(name);
+            }
+        }
     }
 }
