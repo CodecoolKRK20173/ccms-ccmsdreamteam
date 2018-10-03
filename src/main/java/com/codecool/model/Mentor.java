0bc0c7dc58001ffeba9c9235f7f964da89448780
Michal Wisniewski <mihuwis@gmail.com>
2018-10-03 10:29:12 +0200

    Merge with new Dev version

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -2,33 +2,7 @@ package com.codecool.model;
 
 public class Mentor extends Employee {
 
-    public Mentor(){
 
-    }
-
-    public void addAssigmen(){
-
-    }
-
-    public boolean gradeAssigment(boolean isPositiveGrade){
-        return isPositiveGrade;
-    }
-
-    public void addStudent(){
-
-    }
-
-    public void removeStudent(){
-
-    }
-
-    public void editStudent(){
-
-    }
-
-    public void checkAttendance(){
-
-    }
 
 
 }
