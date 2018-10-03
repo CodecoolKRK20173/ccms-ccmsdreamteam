84b23054f5c2fb105a7f2ea395c9a447d0b5d4c8
Marta Zając <xmartza1@gmail.com>
2018-10-03 12:47:10 +0200

    Refactor field assignmentList, methods submitAssignment and getDataAssignmentList to StudentController

--- src/main/java/com/codecool/model/Student.java
+++ src/main/java/com/codecool/model/Student.java
@@ -5,36 +5,9 @@ import java.util.List;
 
 public class Student extends User {
 
-    List<Assignment> assignmentList = new ArrayList<>();
 
     public Student() {
         super();
     }
-
-    public void submitAssigment(String assignmentTittle, String assignmentLink) {
-        Assignment assignment = new Assignment(assignmentTittle, assignmentLink);
-        assignmentList.add(assignment);
-    }
-
-    public List getDataAssigmentList() {
-        List<String> dataAssignmentList = new ArrayList<>();
-        for (Assignment assignment : assignmentList) {
-            dataAssignmentList.add(assignment.getAssignmentTittle());
-            dataAssignmentList.add(assignment.getAssignmentLink());
-            if (assignment.getIsGraded()) {
-                dataAssignmentList.add("Graded");
-                if (assignment.getGrade()) {
-                    dataAssignmentList.add("Passed");
-                } else {
-                    dataAssignmentList.add("Not passed");
-                }
-
-            } else {
-                dataAssignmentList.add("Not Graded");
-            }
-        }
-
-        return dataAssignmentList;
-
-    }
+    
 }
