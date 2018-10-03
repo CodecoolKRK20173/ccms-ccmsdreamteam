83fc224b38b54b8ebc9fb4e826551e165b5edff7
Marta Zając <xmartza1@gmail.com>
2018-10-03 14:11:01 +0200

    Implemnt method printGradedAssigmentForStudent

--- src/main/java/com/codecool/view/View.java
+++ src/main/java/com/codecool/view/View.java
@@ -14,24 +14,42 @@ public class View {
         System.out.println("1.Log In\n2.Exit");
         printGetOption();
     }
-    public void printMenuForUser(String[] userMenu){
+
+    public void printMenuForUser(String[] userMenu) {
         for (int i = 0; i < userMenu.length; i++) {
             System.out.println("(" + (i + 1) + ") " + userMenu[i]);
         }
     }
+
     public void printListOfUsers(List<User> listOfUsers) {
         int num = 1;
         for (User user : listOfUsers) {
             System.out.println("(" + num + ") " + user.getNameOfUser() + " " + user.getSurnameOfUser());
-            num ++;
+            num++;
         }
     }
+
+    public void printGradedAssigmentForStudent(List<String> dataAssignmentList) {
+        int num = 1;
+        for (int i = 0; i < dataAssignmentList.size(); i += 4) {
+            String tittle = dataAssignmentList.get(i);
+            String link = dataAssignmentList.get(i + 1);
+            String status = dataAssignmentList.get(i + 2);
+            String grade = dataAssignmentList.get(i + 3);
+            System.out.println("Tittle: " + tittle + " Link: " + link + " Status: " + status + " Grade: " + grade);
+
+        }
+
+    }
+
     private void printHeader() {
         System.out.println("CcMS - Codecool Management System");
     }
+
     private void printGetOption() {
         System.out.print("Choose option: ");
     }
+
     public int getUserMenuOption() {
         Scanner scanner = new Scanner(System.in);
         int userInput = 0;
@@ -48,9 +66,11 @@ public class View {
         }
         return userInput;
     }
+
     public void printErrorInputMessage() {
         System.out.println("Provide proper input!");
     }
+
     public void printWelcomeUser(String user) {
         System.out.println("Welcome" + user + "!");
     }
