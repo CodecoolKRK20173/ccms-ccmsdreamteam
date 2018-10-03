a819f1a4f3d919478f76c2e2ab5af6fd93bbeeec
Marta Zając <xmartza1@gmail.com>
2018-10-03 12:28:40 +0200

    Add field assignmentList and submitAssigment and getDataAssignment methods

--- src/main/java/com/codecool/model/Assignment.java
+++ src/main/java/com/codecool/model/Assignment.java
@@ -2,27 +2,36 @@ package com.codecool.model;
 
 public class Assignment implements Gradeable {
 
-    private String assigmentTittle;
-    private String assigmentLink;
+    private String assignmentTittle;
+    private String assignmentLink;
     private boolean isGraded;
     private boolean grade;
 
-    @Override
-    public String getAssignmentTittle() {
-        return assigmentTittle;
+    public Assignment(String assignmentTittle, String assignmentLink) {
+        this.assignmentTittle = assignmentTittle;
+        this.assignmentLink = assignmentLink;
     }
 
-    public void setAssigmentTittle(String assigmentTittle){
-        this.assigmentTittle = assigmentTittle;
+    public Assignment(String assignmentTittle) {
+        this.assignmentTittle = assignmentTittle;
+    }
+
+    @Override
+    public String getAssignmentTittle() {
+        return assignmentTittle;
+    }
+
+    public void setAssignmentTittle(String assignmentTittle) {
+        this.assignmentTittle = assignmentTittle;
     }
 
     @Override
     public String getAssignmentLink() {
-        return assigmentLink;
+        return assignmentLink;
     }
 
-    public  void setAssigmentLink(String assigmentLink){
-        this.assigmentLink = assigmentLink;
+    public void setAssignmentLink(String assignmentLink) {
+        this.assignmentLink = assignmentLink;
     }
 
     @Override
@@ -30,7 +39,7 @@ public class Assignment implements Gradeable {
         return isGraded;
     }
 
-    public void setIsGraded(boolean isGraded){
+    public void setIsGraded(boolean isGraded) {
         this.isGraded = isGraded;
     }
 
@@ -39,7 +48,7 @@ public class Assignment implements Gradeable {
         return grade;
     }
 
-    public void setGrade(boolean grade){
+    public void setGrade(boolean grade) {
         this.grade = grade;
     }
 }
--- src/main/java/com/codecool/model/Student.java
+++ src/main/java/com/codecool/model/Student.java
@@ -1,13 +1,40 @@
 package com.codecool.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student extends User {
 
+    List<Assignment> assignmentList = new ArrayList<>();
 
-    public void submitAssigment(){
-
+    public Student() {
+        super();
     }
 
-    public void viewGrades(){
+    public void submitAssigment(String assignmentTittle, String assignmentLink) {
+        Assignment assignment = new Assignment(assignmentTittle, assignmentLink);
+        assignmentList.add(assignment);
+    }
+
+    public List getDataAssigmentList() {
+        List<String> dataAssignmentList = new ArrayList<>();
+        for (Assignment assignment : assignmentList) {
+            dataAssignmentList.add(assignment.getAssignmentTittle());
+            dataAssignmentList.add(assignment.getAssignmentLink());
+            if (assignment.getIsGraded()) {
+                dataAssignmentList.add("Graded");
+                if (assignment.getGrade()) {
+                    dataAssignmentList.add("Passed");
+                } else {
+                    dataAssignmentList.add("Not passed");
+                }
+
+            } else {
+                dataAssignmentList.add("Not Graded");
+            }
+        }
+
+        return dataAssignmentList;
 
     }
 }
