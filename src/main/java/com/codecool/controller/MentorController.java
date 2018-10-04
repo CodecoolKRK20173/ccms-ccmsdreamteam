755b5f145c6f6f51bf3059a8e40e4aa1fbe82629
Michal Wisniewski <mihuwis@gmail.com>
2018-10-04 16:06:09 +0200

    Add changes to MentorController

--- src/main/java/com/codecool/controller/MentorController.java
+++ src/main/java/com/codecool/controller/MentorController.java
@@ -2,20 +2,26 @@ package com.codecool.controller;
 
 import com.codecool.dao.MentorDAO;
 import com.codecool.dao.MentorDAOinter;
+import com.codecool.dao.StudentDAO;
 import com.codecool.model.Assignment;
 import com.codecool.model.Gradeable;
 import com.codecool.model.User;
 import com.codecool.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MentorController {
 
     private MentorDAOinter mentorDAO;
+    private StudentDAO studentDao;
+    private List<Assignment> assignmentList;
     private User mentor;
     private View view;
     private final String[] mentorMenu = {"List students",
                                          "Add assignment",
                                          "Grade assignment",
-                                         "Check attendance",
+                                         "View student assigment",
                                          "Add student",
                                          "Remove student",
                                          "Edit student",
@@ -26,13 +32,15 @@ public class MentorController {
         this.mentor = mentorUser;
         this.mentorDAO = new MentorDAO();
         this.view = new View();
+        this.studentDao = new StudentDAO();
+
     }
 
     public void manageMentor() {
         int listStudentsOption = 1;
         int addAssignmentOption = 2;
         int gradeAssignmentOption = 3;
-        int checkAttendanceOption = 4;
+        int viewStudentsAssigmentOption = 4;
         int addStudentOption = 5;
         int removeStudentOption = 6;
         int editStudentsOption = 7;
@@ -49,8 +57,10 @@ public class MentorController {
             } else if (userMenuOption == addAssignmentOption) {
                 // TODO
             } else if (userMenuOption == gradeAssignmentOption) {
+                gradeAssigment(view.getStringInput(), view.getStringInput(), view.getStringInput());
                 // TODO
-            } else if (userMenuOption == checkAttendanceOption) {
+            } else if (userMenuOption == viewStudentsAssigmentOption) {
+                view.printGradedAssigmentForStudent(viewStudentAssigments(view.getStringInput()));
                 // TODO
             } else if (userMenuOption == addStudentOption) {
                 // TODO
@@ -66,6 +76,33 @@ public class MentorController {
         }
     }
 
+    public List viewStudentAssigments(String login){
+        this.assignmentList = studentDao.loadAssignments(login);
+
+        List<String> dataAssignmentList = new ArrayList<>();
+
+        for (Assignment assignment : assignmentList) {
+            dataAssignmentList.add(assignment.getAssignmentTittle());
+            dataAssignmentList.add(assignment.getAssignmentLink());
+            if (assignment.getIsGraded()) {
+                dataAssignmentList.add("Graded");
+                if (assignment.getGrade()) {
+                    dataAssignmentList.add("Passed");
+                } else {
+                    dataAssignmentList.add("Not passed");
+                }
+
+            } else {
+                dataAssignmentList.add("Not Graded");
+                dataAssignmentList.add("Not passed");
+            }
+        }
+
+        return dataAssignmentList;
+
+
+    }
+
     public void addAssigmen(){
         view.askMentorToSetTitle();
         String assigmentTitle = view.getStringInput();
@@ -77,8 +114,22 @@ public class MentorController {
 
     }
 
-    public boolean gradeAssigment(boolean isPositiveGrade){
-        return isPositiveGrade;
+    public void gradeAssigment(String login, String assignmentTittle, String grade){
+
+        boolean isPased = false;
+
+        this.assignmentList = studentDao.loadAssignments(login);
+        if (grade.equals("Passed")) {
+            isPased = true;
+        }
+
+        for(Assignment assignment: assignmentList){
+            if(assignment.getAssignmentTittle().equals(assignmentTittle)){
+                assignment.setIsGraded(true);
+                assignment.setGrade(isPased);
+                studentDao.updateAssignment(StudentDAO.AssignmentParameters.NOTE, login, grade);
+            }
+        }
     }
 
     public void addStudent(){
@@ -93,7 +144,5 @@ public class MentorController {
 
     }
 
-    public void checkAttendance(){
 
-    }
 }
